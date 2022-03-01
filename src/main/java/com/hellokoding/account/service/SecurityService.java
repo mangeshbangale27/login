71e19bc7b324f241dc472e43d5ef93e1d10f1c0f
Mangesh <Mangeshbangale27@gmail.com>
2022-03-01 17:47:10 +0530

    updated securuty file

--- src/main/java/com/hellokoding/account/service/SecurityService.java
+++ src/main/java/com/hellokoding/account/service/SecurityService.java
@@ -3,5 +3,7 @@ package com.hellokoding.account.service;
 public interface SecurityService {
     String findLoggedInUsername();
 
+// adding comment line
+
     void autologin(String username, String password);
 }
